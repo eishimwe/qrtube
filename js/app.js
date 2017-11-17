b8eee322b1772b93361194975361b56b10c385cc
Elie Ishimwe <Elie.Ishimwe@ignitiongroup.co.za>
2017-11-17 08:19:19 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -218,289 +218,64 @@ Vue.component('home', {
     data: function () {
 
         return {
-            products        :{},
-            provinces       :{},
-            deliveryAddress :{},
-            triple_sims:"0",
-            address_id:"",
-            street_address:"",
-            suburb:"",
-            area:"",
-            city:"",
-            code:"",
-            prov:"",
-            delivery_instruction:"",
-            formData:{},
-            settings:{},
-            cards:{},
-            online_payment:true,
-            selectedCard:"",
-            payment_infos:{},
-            deliveryCost:0,
-            subtotal:0,
-            vat:0,
-            invoiceNo:"",
-            paymentReference:Cookies.get('account_number'),
+
 
 
         }
 
     },
     methods:{
-        get_distributor_products: function(){
 
-            const vm = this;
-            axios.get(BASEURL + 'api/v1/distributorProducts',getCustomHeaders).then(function (response) {
+    },
+    beforeCreate(){
 
-                vm.products = response;
-            });
-        },
-        get_provinces: function(){
-            const vm = this;
-            axios.get(BASEURL + 'api/v1/dropdownlists',getCustomHeaders).then(function (response) {
 
-                vm.provinces = response.data.provinces;
-            })
 
-        },
-        get_global_settings: function(){
-            const vm = this;
-            axios.get(BASEURL + 'api/v1/getGeneralSettings',getCustomHeaders).then(function (response) {
+    },
+    created(){
 
-                vm.settings = response.data.data;
 
-                $.each(vm.settings, function( index, value ) {
 
-                    if(value.delivery_charge){
 
-                        vm.deliveryCost = value.delivery_charge;
 
-                    }
+    }
 
-                    if(value.vat){
+})
 
-                        vm.vat = value.vat;
+Vue.component('createaccount', {
+    template:"#createaccount",
+    data: function () {
 
-                    }
+        return {
 
-
-
-                });
-            })
-
-        },
-        get_delivery_address: function(){
-
-            const vm = this;
-            axios.get(BASEURL + 'api/v1/getDeliveryAddress/'+ Cookies.get("person_id"),getCustomHeaders).then(function (response) {
-
-                vm.street_address  =  response.data.data.street_address;
-                vm.suburb          =  response.data.data.suburb;
-                vm.area            =  response.data.data.area;
-                vm.city            =  response.data.data.city;
-                vm.prov            =  response.data.data.province_id;
-                vm.code            =  response.data.data.code;
-                vm.address_id      =  response.data.data.id;
-
-            });
-
-        },
-        isOnlinePayment: function(response){
-
-            $("#online_payment_value").val(response);
-
-        },
-        calculateOrderTotal: function(){
-
-            var boxes = 0;
-            const vm = this;
-            vm.subtotal = 0;
-
-
-            $('._jsOrderItem').each(function(){
-                vm.subtotal = vm.subtotal + parseFloat($(this).find('option:selected').attr('cost'));
-
-            });
-
-            var delivery    = parseFloat($('#_jsPayDelivery').text());
-            vm.deliveryCost = delivery;
-
-
-            boxes = vm.subtotal/1450;
-            if(boxes > 3){
-                delivery = boxes*20;
-            }
-            // limit sim orders
-            if(boxes > 2){
-                $('._jsSubmitForm').prop('disabled', true);
-                $('#triple_sims').addClass('errorFailed');
-                failAlert('Due to demand, a total maximum of 100 SIMs can be purchased at a time.');
-            }else if(boxes == 0){
-                $('._jsSubmitForm').prop('disabled', true);
-                $('#triple_sims').addClass('errorFailed');
-            }else{
-                $('._jsSubmitForm').prop('disabled', false);
-                $('#triple_sims').removeClass('errorFailed');
-            }
-
-            vm.subtotal = vm.subtotal + delivery;
-
-
-            $('#_jsTotalVAT').text(vm.vat);
-            $('#_jsTotalPRICE').text((vm.subtotal).toFixed(2));
-            $('#_jsPayTotal').text((vm.subtotal).toFixed(2));
-            $('#_jsPayTotal').text((vm.subtotal).toFixed(2));
-            $('*_payDelivery').text((delivery).toFixed(2));
-
-        },
-
-        generateInvoice:function(){
-
-            const vm = this;
-            axios.get(BASEURL + 'api/v1/generateInvoiceLegacy',getCustomHeaders).then(function (response) {
-
-                vm.invoiceNo =  response.data.data.id;
-                vm.processpayment();
-
-            });
-
-
-        },
-        processPreOrder:function(){
-
-            var quantity  = 0;
-            var delivery  = {};
-            var address   = {};
-            var orders    = [];
-            var triple_product   = {};
-            var json_object    = {};
-
-            if(this.triple_sims > 0){
-
-                triple_product.product_id = this.triple_sims;
-
-                if(this.triple_sims == 5){
-
-                    triple_product.quantity   = 50;
-                }
-                if(this.triple_sims == 6){
-
-                    triple_product.quantity   = 100;
-                }
-
-                orders.push(triple_product);
-
-            }
-
-
-            //Delivery
-            delivery.address_id           = this.address_id;
-            delivery.street_address       = this.street_address;
-            delivery.suburb               = this.suburb;
-            delivery.area                 = this.area;
-            delivery.city                 = this.city;
-            delivery.code                 = this.code;
-            delivery.province_id          = this.prov;
-            delivery.delivery_instruction = this.delivery_instruction;
-            delivery.delivery_cost        = this.deliveryCost;
-            delivery.subtotal             = this.subtotal;
-            delivery.vat                  = this.vat;
-
-            //Address
-            address.delivery = delivery;
-
-            //Json Object
-            json_object.orders     = orders;
-            json_object.delivery   = true;
-            json_object.vat        = true;
-            json_object.address    = address;
-            json_object.person_id  = Cookies.get("person_id");
-            this.formData.invoice_no     = vm.invoiceNo;
-            this.formData.json_object    = json_object;
-
-
-            axios.post(BASEURL + 'api/v1/preOrder',this.formData,getCustomHeaders)
-                .then()
-                .catch(error =>{
-
-                console.log(error.response);
-        })
-
-
-
-        },
-        processpayment:function(){
-
-            vm = this;
-            vm.processPreOrder();
-            var paymentInfo                 = {};
-            var responseData                = "";
-            var returnUrl                   = window.location.href.split('#')[0];
-            paymentInfo.token               = vm.selectedCard;
-            paymentInfo.amount              = vm.subtotal * 100;
-            paymentInfo.action              = "distributor_sims";
-            paymentInfo.invoiceId           =  vm.invoiceNo;
-            paymentInfo.returnUrl           =  returnUrl +"#/orderhistory";
-            paymentInfo.person_id           =  Cookies.get("person_id");
-            paymentInfo.email               =  Cookies.get('email');
-
-            $.ajax({
-                type:'post',
-                url: BASEURL + 'api/v1/paynowpayweb',
-                dataType: 'json',
-                data:JSON.stringify(paymentInfo),
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader("mvno", MVNO);
-                },
-                success: function(response) {
-
-                    var responseData = response;
-                    var iFrame = '<form action="' + responseData.url + '" method="post" class="secureForm" style="display:none">' +
-                        '<input type="text" name="PAYGATE_ID" id="PAYGATE_ID" value="' + responseData.parameters.PAYGATE_ID + '">' +
-                        '<input type="text" name="PAY_REQUEST_ID" id="PAY_REQUEST_ID" value="' + responseData.parameters.PAY_REQUEST_ID + '">' +
-                        '<input type="text" name="CHECKSUM" id="CHECKSUM" value="' + responseData.parameters.CHECKSUM + '">' +
-                        '<input type="submit" value="post">' +
-                        '</form>';
-
-                    $(".paygate_form").html(iFrame);
-                    $(".secureForm").submit();
-                    $(".paygate_form").html('');
-
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-
-
-
-                }
-            });
-
-            return responseData;
-
-        },
-
-        processOrder(){
-
-
-            this.generateInvoice();
-
-        },
-        getCards:function(){
-
-            const vm = this;
-            axios.get(BASEURL + 'api/v1/getDistributorCards/'+ distributor_id,getCustomHeaders).then(function (response) {
-
-                vm.cards =  response.data;
-
-            });
-        },
-        addCard:function(){
-
-            var CardNo = this.selectedCard;
-            //TODO:display modal with you card has been change
-
-
+            username:"",
+            email:"",
+            password:"",
+            confirm_password:""
 
         }
+
+    },
+
+    computed:{
+
+        missingUsername:function(){ return this.username === '';},
+        missingEMail:function(){ return this.email === '';},
+        missingPassword:function(){ return this.password === '';},
+        missingConfirmPassword:function(){ return this.confirm_password === '';},
+
+    },
+    methods:{
+
+        validateCreateAccountForm: function (event) {
+            this.attemptSubmit = true;
+            if (this.missingUsername || this.missingEMail || this.missingPassword || this.missingConfirmPassword ){
+                event.preventDefault()
+            }else {
+
+                this.updateBusinessProfile();
+            }
+        },
 
     },
     beforeCreate(){
@@ -527,11 +302,14 @@ Vue.component('home', {
 
 })
 
-const home   = { template: '<home></home>' }
+
+const home             = { template: '<home></home>' }
+const createaccount   = { template: '<createaccount></createaccount>' }
 
 const routes = [
 
-    { path: '/', component: home },
+    { path: '/', component: home},
+    { path: '/createaccount', component: createaccount},
 
 ]
 
